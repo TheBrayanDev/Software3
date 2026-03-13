791e0c783f8efbc590688c3d65c42789d27f48d5
ValeryA26 <andy21211309@gmail.com>
2026-03-13 14:40:09 -0500

    Organización de proyectos

--- DemoAcademicoApplicationFaker/src/main/java/co/edu/demoacademico/service/EstudianteNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package co.edu.demoacademico.service;
-
-public class EstudianteNoEncontradoException extends RuntimeException {
-    public EstudianteNoEncontradoException(String email) {
-        super("No se encontró estudiante con el email: " + email);
-    }
-}
