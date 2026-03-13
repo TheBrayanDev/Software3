113e36944de16ce86a756e8336730fb9a0697a26
ValeryA26 <andy21211309@gmail.com>
2026-03-13 16:17:25 -0500

    Parte 1 del lab (completa) (Parte 2 completa está en la branch con el respectivo nombre)

--- demoacademico/demoacademico/src/main/java/co/edu/demoacademico/exception/EmailYaExisteException.java
+++
@@ -1,7 +0,0 @@
-package co.edu.demoacademico.exception;
-
-public class EmailYaExisteException extends RuntimeException {
-    public EmailYaExisteException(String message) {
-        super(message);
-    }
-}
