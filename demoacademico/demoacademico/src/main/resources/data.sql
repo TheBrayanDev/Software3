e6ad8af32fa39cb38b5a1d434b01b3fe7bb04c46
ValeryA26 <andy21211309@gmail.com>
2026-03-13 14:56:10 -0500

    Organización de proyectos

--- demoacademico/demoacademico/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO estudiante (nombre, email) VALUES ('Ana Pérez', 'ana@demo.com');
-INSERT INTO estudiante (nombre, email) VALUES ('Juan Gómez', 'juan@demo.com');
-INSERT INTO estudiante (nombre, email) VALUES ('Carlos Ruiz', 'carlos@demo.com');
